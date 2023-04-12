a442f2e0fbd78272c3aed1ea5ef6d692c2bd9880
mahirb22 <codeitout22@gmail.com>
2023-04-12 19:09:36 -0400

    First

---
+++ create-clean-vite-react-ts.js
@@ -0,0 +1,64 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+// Set the project name and template
+const projectName = process.argv[2] || "my-vite-react-ts-app";
+const template = "react-ts";
+
+console.log(`Creating a new Vite React TypeScript project: ${projectName}`);
+
+// Execute the create-vite command to create a new Vite project
+execSync(`create-vite ${projectName} --template ${template}`, {
+  stdio: "inherit",
+});
+
+// Set the project directory path
+const projectDir = path.resolve(process.cwd(), projectName);
+
+// List the files and folders to remove
+const filesToRemove = [
+  ".gitignore",
+  "src/logo.svg",
+  "src/App.css",
+  "src/components/HelloWorld.module.css",
+  "src/assets",
+];
+
+// Remove the files and folders listed in filesToRemove
+filesToRemove.forEach((file) => {
+  const filePath = path.resolve(projectDir, file);
+  if (fs.existsSync(filePath)) {
+    fs.rmSync(filePath, { recursive: true, force: true });
+  }
+});
+
+// Update the content of App.tsx
+const appFilePath = path.resolve(projectDir, "src/App.tsx");
+const newAppContent = `
+
+function App() {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
+
+export default App;
+`;
+
+// Write the new content to App.tsx
+fs.writeFileSync(appFilePath, newAppContent, { encoding: "utf-8" });
+
+// Clear the content of index.css
+const indexCSSPath = path.resolve(projectDir, "src/index.css");
+fs.writeFileSync(indexCSSPath, "", { encoding: "utf-8" });
+
+// Install react and react-dom as dependencies
+execSync(`cd ${projectName} && npm install react react-dom`);
+
+// Install @types/react and @types/react-dom as development dependencies
+execSync(
+  `cd ${projectName} && npm install --save-dev @types/react @types/react-dom`
+);
